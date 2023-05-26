15ed4c7d03c8c6a64de21aaa7264943dfb2ced50
Jesse Giammattolo <giammattolo@hotmail.com>
2023-05-26 10:36:17 -0400

    added social media share buttons

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const navbar = document.querySelector('.navbar')
-const  sticky = navbar.offsetTop;
+const sticky = navbar.offsetTop
 const topBTN = document.querySelector('.top-btn')
 const contactSection = document.getElementById('contact')
 
@@ -8,20 +8,19 @@ window.addEventListener('scroll', navOpacity)
 window.addEventListener('scroll', topScroll)
 topBTN.addEventListener('click', scrollToTop)
 
-
 // Navbar opacity functions
-function navOpacity(){
-  if(window.scrollY > 150){
+function navOpacity() {
+  if (window.scrollY > 150) {
     navbar.style.opacity = 0.9
-  }else{
+  } else {
     navbar.style.opacity = 1
   }
 }
 
 function navScroll() {
-  if(window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky')
-  }else{
+  } else {
     navbar.classList.remove('sticky')
   }
 }
@@ -30,64 +29,83 @@ function navScroll() {
 function scrollToTop() {
   // document.body.scrollTop = 0;
   // document.documentElement.scrollTop = 0
-  contactSection.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+  contactSection.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    inline: 'nearest',
+  })
 }
 
 function topScroll() {
-  if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
     topBTN.style.display = 'block'
-  }else{
+  } else {
     topBTN.style.display = 'none'
   }
 }
 
-// 
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages;    
+//
+document.addEventListener('DOMContentLoaded', function () {
+  var lazyloadImages
 
-  if ("IntersectionObserver" in window) {
-    lazyloadImages = document.querySelectorAll(".lazy");
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
+  if ('IntersectionObserver' in window) {
+    lazyloadImages = document.querySelectorAll('.lazy')
+    var imageObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
         if (entry.isIntersecting) {
-          var image = entry.target;
-          image.src = image.dataset.src;
-          image.classList.remove("lazy");
-          imageObserver.unobserve(image);
+          var image = entry.target
+          image.src = image.dataset.src
+          image.classList.remove('lazy')
+          imageObserver.unobserve(image)
         }
-      });
-    });
+      })
+    })
 
-    lazyloadImages.forEach(function(image) {
-      imageObserver.observe(image);
-    });
-  } else {  
-    var lazyloadThrottleTimeout;
-    lazyloadImages = document.querySelectorAll(".lazy");
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
+    lazyloadImages.forEach(function (image) {
+      imageObserver.observe(image)
+    })
+  } else {
+    var lazyloadThrottleTimeout
+    lazyloadImages = document.querySelectorAll('.lazy')
 
-      lazyloadThrottleTimeout = setTimeout(function() {
-        var scrollTop = window.pageYOffset;
-        lazyloadImages.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
-        });
-        if(lazyloadImages.length == 0) { 
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout)
+      }
+
+      lazyloadThrottleTimeout = setTimeout(function () {
+        var scrollTop = window.pageYOffset
+        lazyloadImages.forEach(function (img) {
+          if (img.offsetTop < window.innerHeight + scrollTop) {
+            img.src = img.dataset.src
+            img.classList.remove('lazy')
+          }
+        })
+        if (lazyloadImages.length == 0) {
+          document.removeEventListener('scroll', lazyload)
+          window.removeEventListener('resize', lazyload)
+          window.removeEventListener('orientationChange', lazyload)
         }
-      }, 20);
+      }, 20)
     }
 
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
+    document.addEventListener('scroll', lazyload)
+    window.addEventListener('resize', lazyload)
+    window.addEventListener('orientationChange', lazyload)
   }
 })
+
+// social media
+const link = encodeURI(window.location.href)
+const msg = encodeURIComponent('Check out this blog by Jesse James Photography')
+const title = encodeURIComponent(document.querySelector('title').textContent)
+
+console.log([link, msg, title])
+const fb = document.querySelector('.facebook')
+fb.href = `https://www.facebook.com/share.php?u=${link}`
+
+const twitter = document.querySelector('.twitter')
+twitter.href = `http://twitter.com/share?url=${link}&text=${msg}`
--- blog.js
+++ blog.js
@@ -1,5 +1,5 @@
 const navbar = document.querySelector('.navbar')
-const  sticky = navbar.offsetTop;
+const sticky = navbar.offsetTop
 const topBTN = document.querySelector('.top-btn')
 const contactSection = document.getElementById('contact')
 
@@ -7,18 +7,18 @@ window.addEventListener('scroll', navScroll)
 window.addEventListener('scroll', navOpacity)
 
 // Navbar opacity functions
-function navOpacity(){
-  if(window.scrollY > 150){
+function navOpacity() {
+  if (window.scrollY > 150) {
     navbar.style.opacity = 0.9
-  }else{
+  } else {
     navbar.style.opacity = 1
   }
 }
 
 function navScroll() {
-  if(window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky')
-  }else{
+  } else {
     navbar.classList.remove('sticky')
   }
 }
@@ -27,58 +27,72 @@ function navScroll() {
 function scrollToTop() {
   // document.body.scrollTop = 0;
   // document.documentElement.scrollTop = 0
-  contactSection.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+  contactSection.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    inline: 'nearest',
+  })
 }
 
+//
+document.addEventListener('DOMContentLoaded', function () {
+  var lazyloadImages
 
-
-// 
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages;    
-
-  if ("IntersectionObserver" in window) {
-    lazyloadImages = document.querySelectorAll(".lazy");
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
+  if ('IntersectionObserver' in window) {
+    lazyloadImages = document.querySelectorAll('.lazy')
+    var imageObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
         if (entry.isIntersecting) {
-          var image = entry.target;
-          image.src = image.dataset.src;
-          image.classList.remove("lazy");
-          imageObserver.unobserve(image);
+          var image = entry.target
+          image.src = image.dataset.src
+          image.classList.remove('lazy')
+          imageObserver.unobserve(image)
         }
-      });
-    });
+      })
+    })
 
-    lazyloadImages.forEach(function(image) {
-      imageObserver.observe(image);
-    });
-  } else {  
-    var lazyloadThrottleTimeout;
-    lazyloadImages = document.querySelectorAll(".lazy");
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
+    lazyloadImages.forEach(function (image) {
+      imageObserver.observe(image)
+    })
+  } else {
+    var lazyloadThrottleTimeout
+    lazyloadImages = document.querySelectorAll('.lazy')
 
-      lazyloadThrottleTimeout = setTimeout(function() {
-        var scrollTop = window.pageYOffset;
-        lazyloadImages.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
-        });
-        if(lazyloadImages.length == 0) { 
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout)
+      }
+
+      lazyloadThrottleTimeout = setTimeout(function () {
+        var scrollTop = window.pageYOffset
+        lazyloadImages.forEach(function (img) {
+          if (img.offsetTop < window.innerHeight + scrollTop) {
+            img.src = img.dataset.src
+            img.classList.remove('lazy')
+          }
+        })
+        if (lazyloadImages.length == 0) {
+          document.removeEventListener('scroll', lazyload)
+          window.removeEventListener('resize', lazyload)
+          window.removeEventListener('orientationChange', lazyload)
         }
-      }, 20);
+      }, 20)
     }
 
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
+    document.addEventListener('scroll', lazyload)
+    window.addEventListener('resize', lazyload)
+    window.addEventListener('orientationChange', lazyload)
   }
 })
+
+// social media
+const link = encodeURI(window.location.href)
+const msg = encodeURIComponent('Check out this blog by Jesse James Photography')
+const title = encodeURIComponent(document.querySelector('title').textContent)
+
+console.log([link, msg, title])
+const fb = document.querySelector('.facebook')
+fb.href = `https://www.facebook.com/share.php?u=${link}`
+
+const twitter = document.querySelector('.twitter')
+twitter.href = `http://twitter.com/share?url=${link}&text=${msg}`
